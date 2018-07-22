9bffeaa00d910af35d00f3d5df198ae7e2428c78
James <jaimea111@gmail.com>
2018-07-22 03:14:04 -0500

    Creating Sheets in MS Excel 97-2003

--- src/main/java/co/cleversystems/poi/view/Main.java
+++ src/main/java/co/cleversystems/poi/view/Main.java
@@ -10,6 +10,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 /**
@@ -20,10 +21,12 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         /* 
         This will create an excel file on the current project directory. The Excel file will be
-        in a 97-2003 format since is uses the HSSFWorkbook.
+        in a 97-2003 format since it uses the HSSFWorkbook.
         */
         Workbook wb = new HSSFWorkbook(); 
         try(OutputStream fileOut = new FileOutputStream("TheNameOfYourFile.xls")){
+            Sheet sheet1 = wb.createSheet("Your First Sheet");  
+            Sheet sheet2 = wb.createSheet("Your Second Sheet");  
             wb.write(fileOut);  
         } catch(Exception e){
             System.out.println(e.getMessage());
