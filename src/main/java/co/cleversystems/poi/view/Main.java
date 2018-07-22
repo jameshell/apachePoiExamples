c7dcc53204c151a0227172c92e81d97858cfe46d
James <jaimea111@gmail.com>
2018-07-22 14:32:35 -0500

    MS Excel 2007+ file code and dependecy update.

--- src/main/java/co/cleversystems/poi/view/Main.java
+++ src/main/java/co/cleversystems/poi/view/Main.java
@@ -14,6 +14,7 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -23,10 +24,18 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         /* 
         This will create an excel file on the current project directory. The Excel file will be
-        in a 97-2003 format since it uses the HSSFWorkbook.
+        in a 97-2003 (.xls) format since it uses the HSSFWorkbook().
         */
-        Workbook wb = new HSSFWorkbook(); 
-        try(OutputStream fileOut = new FileOutputStream("TheNameOfYourFile.xls")){
+       //Workbook wb = new HSSFWorkbook(); 
+        
+        
+        /*
+        This will create an excel file on the current project directory. The Excel file will be 
+        in a 2007+ (.xlsx)format since is uses XSSFWorkbook().
+        */
+        Workbook wb= new XSSFWorkbook();
+        
+        try(OutputStream fileOut = new FileOutputStream("TheNameOfYourFile.xlsx")){
             
             //The object Sheet will create allow the creation of sheets.
             Sheet sheet1 = wb.createSheet("Your First Sheet");
